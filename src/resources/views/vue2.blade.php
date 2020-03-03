f235c4c2eeac0aa19f66467077c310a39c988b0c
Florian CELLIER <florian.cellier@dedi-agency.com>
2020-03-03 11:26:18 +0100

    chore(tutoriel): a try with route

---
+++ src/resources/views/vue2.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Ma première vue</title>
+</head>
+<body>
+<div>Je suis sur la page <?php echo $num; ?> </div>
+<div>Je suis le deuxième paramêtre <?php echo $secondArg; ?></div>
+<div>Je suis sur le troisieme paramêtre <?php echo $thirdArg; ?></div>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('{n}/{t?}', [
+    'as' => 'number',
+    fn ($n, $t = null) => 'Je suis sur la page ' . $n . ':' . $t ??= 'Rien dans t'
+])
+    ->where('n', '[1-3]')
+;
+
+Route::get('/foo', [
+    'as' => 'foo',
+     fn () => view('vue1')
+]);
+
+Route::get('/bar/{n}/{f}/{b}', [
+    'as' => 'bar',
+    fn ($n, $f, $b) => view('vue2', ['thirdArg' => $b])
+        ->with('num', $n)
+        ->withSecondArg($f)
+]);
+
+//$this->app['router']->get('/foo', fn () => 'foo'); // Not work as app is undefined
